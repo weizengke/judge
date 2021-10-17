3c12b23701f610bd69a08474df109f8f813cebee
jungle <269574524@qq.com>
2021-10-17 22:38:53 +0800

    ci

--- upload.bat
+++ upload.bat
@@ -2,7 +2,7 @@
 echo open 139.159.160.103>> ftp.txt
 echo ci>> ftp.txt
 echo online-judge-ci>> ftp.txt
-echo put C:\Users\travis\build\weizengke\judge\build\VSBuild\bin\Release\judger.exe>> ftp.txt
+echo put build\VSBuild\bin\Release\judger.exe>> ftp.txt
 echo bye>> ftp.txt
 FTP -s:ftp.txt
 del ftp.txt /q
