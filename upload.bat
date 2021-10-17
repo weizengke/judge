b1128355e8b48a01b8feebeee86991b647e5a7ef
jungle <269574524@qq.com>
2021-10-17 22:05:39 +0800

    ci

--- upload.bat
+++ upload.bat
@@ -1,9 +1,7 @@
 @Echo Off
 Echo open 139.159.160.103 >ftp.up
-Echo %1>>ftp.up
-Echo %2>>ftp.up
-Echo Cd .\ >>ftp.up
-Echo binary>>ftp.up
+Echo w00583872>>ftp.up
+Echo wzk!1989>>ftp.up
 Echo put "C:\Users\travis\build\weizengke\judge\build\VSBuild\bin\Release\judger.exe">>ftp.up
 Echo bye>>ftp.up
 FTP -s:ftp.up
